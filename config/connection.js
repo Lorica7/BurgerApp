6c4c1e228e0b4e559ca88286babdf888140d6972
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2018-08-23 10:56:20 -0400

    JAWSdb info added

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,17 @@
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL){
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+ }  else {
+   connection = mysql.createConnection({
+     host: 'localhost',
+     user: 'root',
+     password: 'root',
+     database: "burgers_db"
+   });
+ }
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 8889,
@@ -15,6 +27,7 @@ connection.connect(function(error) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
 
 
 module.exports = connection;
