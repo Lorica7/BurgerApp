e98d96c7f5db3e5a94a0aa02880a9ab05a61e309
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2018-08-24 11:51:47 -0400

    changed file for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,33 +1,32 @@
-var mysql = require("mysql");
+var mysql = require('mysql'); 
 
-if (process.env.JAWSDB_URL){
-connection = mysql.createConnection(process.env.JAWSDB_URL);
 
- }  else {
-   connection = mysql.createConnection({
-     host: 'localhost',
-     user: 'root',
-     password: 'root',
-     database: "burgers_db"
-   });
- }
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // DB is local on localhost
+    connection = mysql.createConnection({
+        port: 8889,
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'burgers_db'
+    });
+}
 
-connection.connect(function(error) {
-  if (error) {
-    console.error("error connecting: " + error.stack);
+
+
+connection.connect(function(err) 
+{
+  if (err) 
+  {
+    console.error('error connecting: ' + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log('connected as id ' + connection.threadId);
 });
-
-
 
 module.exports = connection;
